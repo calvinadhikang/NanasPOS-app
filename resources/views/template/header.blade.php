0303fd0a329bca0dc0d7d7d9073f517fdecbb297
calvinadhikang <calvinadhikang@gmail.com>
2023-07-03 02:38:36 +0700

    change Title on HTML

--- resources/views/template/header.blade.php
+++ resources/views/template/header.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Nanas Pos App</title>
     @vite('resources/css/app.css')
     <script src="https://kit.fontawesome.com/7ecee868f3.js" crossorigin="anonymous"></script>
 </head>
