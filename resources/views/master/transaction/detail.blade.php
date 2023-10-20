7fc6acbdd0ad5e529c75346fc662aa08e4f3a177
Calvin Adhikang <calvinadhikang@gmail.com>
2023-10-20 11:15:05 +0700

    fix detail transaction view

--- resources/views/master/transaction/detail.blade.php
+++ resources/views/master/transaction/detail.blade.php
@@ -4,7 +4,7 @@
 <div class="prose">
     <h1 class="text-4xl font-bold mb-4">Detail Transaksi</h1>
 </div>
-<div class="rounded bg-accent p-4 my-5 shado">
+<div class="rounded bg-stone-200 p-10 my-5 shadow-md">
     <?php
     $status = 'bg-green-500';
     $teks = "Lunas";
@@ -16,25 +16,28 @@
     <div class="{{ $status }} p-4 rounded-lg">
         <h1 class="text-xl font-bold text-center rounded text-white">{{ $teks }}</h1>
     </div>
-    <br>
-    <div class="flex flex-wrap justify-between">
-        <div class="basis-3/5 bg-secondary text-white p-4 rounded-md shadow-lg">
-            <div class="text-2xl font-medium">Nomor Nota : {{ $trans->id }}</div>
-            <div class="text-xl font-medium">Nama Customer : {{ $trans->customer }}</div>
-        </div>
-        <div class="basis-2/6 bg-secondary text-white p-4 rounded-md shadow-lg">
-            <div class="text-xl">Grand Total: Rp {{ number_format($trans->grandtotal) }}</div>
-            <div class="">Diskon: Rp {{ number_format($trans->diskon) }}</div>
-            <div class="">{{ $trans->user->nama }}</div>
-        </div>
+    <h1 class="text-xl font-bold mt-5">Detail Transaksi</h1>
+    <div class="grid grid-cols-2">
+        <h1 class="text-lg">Nomor Nota</h1>
+        <h1 class="text-lg font-medium text-right">{{ $trans->id }}</h1>
+        <h1 class="text-lg">Nama Customer</h1>
+        <h1 class="text-lg font-medium text-right">{{ $trans->customer }}</h1>
+        <h1 class="text-lg">Tanggal Transaksi</h1>
+        <h1 class="text-lg font-medium text-right">{{ date_format($trans->created_at, 'd M Y') }}</h1>
     </div>
-    <br>
-    <h1 class="text-3xl">List Pesanan</h1>
+    <div class="divider"></div>
+    <div class="grid grid-cols-2 mt-2">
+        <h1>Diskon</h1>
+        <h1 class="text-right">Rp {{ number_format($trans->diskon) }}</h1>
+        <h1 class="text-lg">Grand Total</h1>
+        <h1 class="text-lg font-medium text-right">Rp {{ number_format($trans->grandtotal) }}</h1>
+    </div>
+    <h1 class="text-xl font-bold mt-10">List Pesanan</h1>
     @foreach ($trans->dtrans as $detail)
-        <div class="py-2 rounded-md my-2 px-2 bg-green-100">
+        <div class="my-2 pb-2 border-b border-b-gray-400">
             <div class="flex flex-wrap">
                 <div class="grow">
-                    <div class="font-bold">{{ $detail->nama }}</div>
+                    <div class="font-semibold">{{ $detail->nama }}</div>
                     <div class="">Rp {{ number_format($detail->harga) }} x {{ $detail->qty }}</div>
                 </div>
                 <div class="pt-4">
